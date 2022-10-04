9000873ef0f269690929632b43b33a4ac7e33181
Jory AlJuhani <jory.aljuhani@kcl.ac.uk>
2022-10-04 13:32:59 +0100

    Trial 2

--- frequencies.py
+++ frequencies.py
@@ -2,8 +2,13 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+    stringitem=[];
+    for i in items:
+        stringitem.append(str(i))
+
     frequencies = {}
-    count=0
-    for item in items:
-        count=items.count(item);
-        frequencies[item]=count;
+    count = 0
+    for item in stringitem:
+        count= stringitem.count(item)
+        frequencies[item]=count
+    return frequencies
